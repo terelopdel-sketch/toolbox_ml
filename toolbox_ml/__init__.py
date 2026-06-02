7eac6bb20bd69f36908122529af08198012d0811
terelopdel-sketch <terelopdel@gmail.com>
2026-06-02 20:50:50 +0200

    Creación de estructura inicial del paquete toolblox_ml

---
+++ toolbox_ml/__init__.py
@@ -0,0 +1,8 @@
+from toolbox_ml.eda.core import (
+    describe_df,
+    tipifica_variables,
+    get_features_num_regression,
+    plot_features_num_regression,
+    get_features_cat_regression,
+    plot_features_cat_regression
+)
---
+++ toolbox_ml/eda/core.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+
+def describe_df()
+    pass
+
+
+def tipifica_variables()
+    pass
+
+
+def get_features_num_regression()
+    pass
+
+
+def plot_features_num_regression()
+    pass
+
+
+def get_features_cat_regression()
+    pass
+
+
+def plot_features_cat_regression()
+    pass
